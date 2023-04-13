6f67c6f540bbf4aeaf9ed9f6f20e6b158a7b4a1e
Jose Carlos <ljosecarlos295@gmail.com>
2023-04-12 23:41:50 -0500

    Consumir informacion de la BD

--- sisventas/Presentacion/frmcliente.Designer.vb
+++ sisventas/Presentacion/frmcliente.Designer.vb
@@ -23,31 +23,31 @@ Partial Class frmcliente
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.txtIdCliente = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtNombres = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtApellidos = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.txtDireccion = New System.Windows.Forms.TextBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.txtDNI = New System.Windows.Forms.TextBox()
-        Me.btnNuevo = New System.Windows.Forms.Button()
-        Me.btnEditar = New System.Windows.Forms.Button()
-        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtApellidos = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNombres = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtIdCliente = New System.Windows.Forms.TextBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.cboCampo = New System.Windows.Forms.ComboBox()
-        Me.txtBuscar = New System.Windows.Forms.TextBox()
-        Me.Label7 = New System.Windows.Forms.Label()
         Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.inexistente = New System.Windows.Forms.Label()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.cboCampo = New System.Windows.Forms.ComboBox()
+        Me.dataListado = New System.Windows.Forms.DataGridView()
         Me.Eliminar = New System.Windows.Forms.DataGridViewCheckBoxColumn()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dataListado, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -76,85 +76,38 @@ Partial Class frmcliente
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Mantenimiento"
         '
-        'txtIdCliente
+        'btnCancelar
         '
-        Me.txtIdCliente.Location = New System.Drawing.Point(172, 43)
-        Me.txtIdCliente.Name = "txtIdCliente"
-        Me.txtIdCliente.Size = New System.Drawing.Size(152, 20)
-        Me.txtIdCliente.TabIndex = 0
+        Me.btnCancelar.BackColor = System.Drawing.Color.Teal
+        Me.btnCancelar.ForeColor = System.Drawing.Color.White
+        Me.btnCancelar.Location = New System.Drawing.Point(249, 346)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 14
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
         '
-        'Label1
+        'btnEditar
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(38, 50)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(57, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "IdCliente"
+        Me.btnEditar.BackColor = System.Drawing.Color.Teal
+        Me.btnEditar.ForeColor = System.Drawing.Color.White
+        Me.btnEditar.Location = New System.Drawing.Point(145, 346)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEditar.TabIndex = 13
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = False
         '
-        'Label2
+        'btnNuevo
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(38, 95)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(56, 13)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Nombres"
-        '
-        'txtNombres
-        '
-        Me.txtNombres.Location = New System.Drawing.Point(172, 88)
-        Me.txtNombres.Name = "txtNombres"
-        Me.txtNombres.Size = New System.Drawing.Size(152, 20)
-        Me.txtNombres.TabIndex = 2
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(38, 141)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(58, 13)
-        Me.Label3.TabIndex = 5
-        Me.Label3.Text = "Apellidos"
-        '
-        'txtApellidos
-        '
-        Me.txtApellidos.Location = New System.Drawing.Point(172, 134)
-        Me.txtApellidos.Name = "txtApellidos"
-        Me.txtApellidos.Size = New System.Drawing.Size(152, 20)
-        Me.txtApellidos.TabIndex = 4
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(38, 190)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(61, 13)
-        Me.Label4.TabIndex = 7
-        Me.Label4.Text = "Direccion"
-        '
-        'txtDireccion
-        '
-        Me.txtDireccion.Location = New System.Drawing.Point(172, 183)
-        Me.txtDireccion.Name = "txtDireccion"
-        Me.txtDireccion.Size = New System.Drawing.Size(152, 20)
-        Me.txtDireccion.TabIndex = 6
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(38, 239)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(57, 13)
-        Me.Label5.TabIndex = 9
-        Me.Label5.Text = "Telefono"
-        '
-        'txtTelefono
-        '
-        Me.txtTelefono.Location = New System.Drawing.Point(172, 232)
-        Me.txtTelefono.Name = "txtTelefono"
-        Me.txtTelefono.Size = New System.Drawing.Size(152, 20)
-        Me.txtTelefono.TabIndex = 8
+        Me.btnNuevo.BackColor = System.Drawing.Color.Teal
+        Me.btnNuevo.ForeColor = System.Drawing.Color.White
+        Me.btnNuevo.Location = New System.Drawing.Point(41, 346)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 12
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = False
         '
         'Label6
         '
@@ -172,47 +125,94 @@ Partial Class frmcliente
         Me.txtDNI.Size = New System.Drawing.Size(152, 20)
         Me.txtDNI.TabIndex = 10
         '
-        'btnNuevo
+        'Label5
         '
-        Me.btnNuevo.BackColor = System.Drawing.Color.Teal
-        Me.btnNuevo.ForeColor = System.Drawing.Color.White
-        Me.btnNuevo.Location = New System.Drawing.Point(41, 346)
-        Me.btnNuevo.Name = "btnNuevo"
-        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
-        Me.btnNuevo.TabIndex = 12
-        Me.btnNuevo.Text = "Nuevo"
-        Me.btnNuevo.UseVisualStyleBackColor = False
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(38, 239)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Telefono"
         '
-        'btnEditar
+        'txtTelefono
         '
-        Me.btnEditar.BackColor = System.Drawing.Color.Teal
-        Me.btnEditar.ForeColor = System.Drawing.Color.White
-        Me.btnEditar.Location = New System.Drawing.Point(145, 346)
-        Me.btnEditar.Name = "btnEditar"
-        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
-        Me.btnEditar.TabIndex = 13
-        Me.btnEditar.Text = "Editar"
-        Me.btnEditar.UseVisualStyleBackColor = False
+        Me.txtTelefono.Location = New System.Drawing.Point(172, 232)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(152, 20)
+        Me.txtTelefono.TabIndex = 8
         '
-        'btnCancelar
+        'Label4
         '
-        Me.btnCancelar.BackColor = System.Drawing.Color.Teal
-        Me.btnCancelar.ForeColor = System.Drawing.Color.White
-        Me.btnCancelar.Location = New System.Drawing.Point(249, 346)
-        Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
-        Me.btnCancelar.TabIndex = 14
-        Me.btnCancelar.Text = "Cancelar"
-        Me.btnCancelar.UseVisualStyleBackColor = False
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 190)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Direccion"
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(172, 183)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(152, 20)
+        Me.txtDireccion.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Apellidos"
+        '
+        'txtApellidos
+        '
+        Me.txtApellidos.Location = New System.Drawing.Point(172, 134)
+        Me.txtApellidos.Name = "txtApellidos"
+        Me.txtApellidos.Size = New System.Drawing.Size(152, 20)
+        Me.txtApellidos.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nombres"
+        '
+        'txtNombres
+        '
+        Me.txtNombres.Location = New System.Drawing.Point(172, 88)
+        Me.txtNombres.Name = "txtNombres"
+        Me.txtNombres.Size = New System.Drawing.Size(152, 20)
+        Me.txtNombres.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "IdCliente"
+        '
+        'txtIdCliente
+        '
+        Me.txtIdCliente.Location = New System.Drawing.Point(172, 43)
+        Me.txtIdCliente.Name = "txtIdCliente"
+        Me.txtIdCliente.Size = New System.Drawing.Size(152, 20)
+        Me.txtIdCliente.TabIndex = 0
         '
         'GroupBox2
         '
         Me.GroupBox2.BackColor = System.Drawing.Color.White
         Me.GroupBox2.Controls.Add(Me.btnEliminar)
-        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.inexistente)
         Me.GroupBox2.Controls.Add(Me.txtBuscar)
         Me.GroupBox2.Controls.Add(Me.cboCampo)
-        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Controls.Add(Me.dataListado)
         Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.GroupBox2.ForeColor = System.Drawing.Color.SteelBlue
         Me.GroupBox2.Location = New System.Drawing.Point(405, 42)
@@ -221,42 +221,6 @@ Partial Class frmcliente
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Listado de Clientes"
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Eliminar})
-        Me.DataGridView1.Location = New System.Drawing.Point(18, 88)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.DataGridView1.Size = New System.Drawing.Size(338, 232)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'cboCampo
-        '
-        Me.cboCampo.FormattingEnabled = True
-        Me.cboCampo.Items.AddRange(New Object() {"nombre", "apellidos", "dni", "telefono"})
-        Me.cboCampo.Location = New System.Drawing.Point(18, 47)
-        Me.cboCampo.Name = "cboCampo"
-        Me.cboCampo.Size = New System.Drawing.Size(133, 21)
-        Me.cboCampo.TabIndex = 1
-        Me.cboCampo.Text = "dni"
-        '
-        'txtBuscar
-        '
-        Me.txtBuscar.Location = New System.Drawing.Point(169, 47)
-        Me.txtBuscar.Name = "txtBuscar"
-        Me.txtBuscar.Size = New System.Drawing.Size(187, 20)
-        Me.txtBuscar.TabIndex = 2
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(131, 210)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(112, 13)
-        Me.Label7.TabIndex = 3
-        Me.Label7.Text = "Datos Inexistentes"
         '
         'btnEliminar
         '
@@ -268,6 +232,42 @@ Partial Class frmcliente
         Me.btnEliminar.TabIndex = 15
         Me.btnEliminar.Text = "Eliminar"
         Me.btnEliminar.UseVisualStyleBackColor = False
+        '
+        'inexistente
+        '
+        Me.inexistente.AutoSize = True
+        Me.inexistente.Location = New System.Drawing.Point(131, 210)
+        Me.inexistente.Name = "inexistente"
+        Me.inexistente.Size = New System.Drawing.Size(112, 13)
+        Me.inexistente.TabIndex = 3
+        Me.inexistente.Text = "Datos Inexistentes"
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(169, 47)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(187, 20)
+        Me.txtBuscar.TabIndex = 2
+        '
+        'cboCampo
+        '
+        Me.cboCampo.FormattingEnabled = True
+        Me.cboCampo.Items.AddRange(New Object() {"nombre", "apellidos", "dni", "telefono"})
+        Me.cboCampo.Location = New System.Drawing.Point(18, 47)
+        Me.cboCampo.Name = "cboCampo"
+        Me.cboCampo.Size = New System.Drawing.Size(133, 21)
+        Me.cboCampo.TabIndex = 1
+        Me.cboCampo.Text = "dni"
+        '
+        'dataListado
+        '
+        Me.dataListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataListado.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Eliminar})
+        Me.dataListado.Location = New System.Drawing.Point(18, 88)
+        Me.dataListado.Name = "dataListado"
+        Me.dataListado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dataListado.Size = New System.Drawing.Size(338, 232)
+        Me.dataListado.TabIndex = 0
         '
         'Eliminar
         '
@@ -288,7 +288,7 @@ Partial Class frmcliente
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dataListado, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -311,8 +311,8 @@ Partial Class frmcliente
     Friend WithEvents btnNuevo As Windows.Forms.Button
     Friend WithEvents GroupBox2 As Windows.Forms.GroupBox
     Friend WithEvents cboCampo As Windows.Forms.ComboBox
-    Friend WithEvents DataGridView1 As Windows.Forms.DataGridView
-    Friend WithEvents Label7 As Windows.Forms.Label
+    Friend WithEvents dataListado As Windows.Forms.DataGridView
+    Friend WithEvents inexistente As Windows.Forms.Label
     Friend WithEvents txtBuscar As Windows.Forms.TextBox
     Friend WithEvents btnEliminar As Windows.Forms.Button
     Friend WithEvents Eliminar As Windows.Forms.DataGridViewCheckBoxColumn
--- sisventas/Presentacion/frmcliente.vb
+++ sisventas/Presentacion/frmcliente.vb
@@ -1,9 +1,69 @@
 ﻿Public Class frmcliente
 
-
-
-
+    Private dt As New DataTable
     Private Sub frmcliente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mostrar()
+    End Sub
 
+
+    Private Sub mostrar()
+
+        Try
+            Dim func As New fcliente
+            dt = func.mostrar
+            dataListado.Columns.Item("Eliminar").Visible = False
+
+            If dt.Rows.Count <> 0 Then
+                dataListado.DataSource = dt
+                txtBuscar.Enabled = True
+                dataListado.ColumnHeadersVisible = True
+                inexistente.Visible = True
+
+            Else
+                dataListado.DataSource = Nothing
+                txtBuscar.Enabled = False
+                dataListado.ColumnHeadersVisible = False
+                inexistente.Visible = False
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+        btnNuevo.Visible = True
+        btnEditar.Visible = False
+
+        buscar()
+
+    End Sub
+
+
+    Private Sub buscar()
+
+        Try
+            Dim ds As New DataSet
+            ds.Tables.Add(dt.Copy)
+            Dim dv As New DataView(ds.Tables(0))
+
+            dv.RowFilter = cboCampo.Text & " like '" & txtBuscar.Text & "%'"
+
+            If dv.Count <> 0 Then
+                inexistente.Visible = False
+                dataListado.DataSource = dv
+                ocultar_columnas()
+            Else
+                inexistente.Visible = True
+                dataListado.DataSource = Nothing
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub ocultar_columnas()
+        dataListado.Columns(1).Visible = False
     End Sub
 End Class
