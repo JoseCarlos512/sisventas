918bbcfc31d65543b5fac0be951c3fd3d30220d9
ljose <ljose@DESKTOP-7NFI325>
2023-04-30 03:17:48 -0500

    Add class producto and functions that permition conect to database

---
+++ SistemaVentasVB/sisventas/Datos/fproducto.vb
@@ -0,0 +1,120 @@
+﻿Imports System.Data.SqlClient
+Public Class fproducto
+
+    'Herencia de la clase base
+    Inherits conexion
+    Dim cmd As New SqlCommand
+
+    Public Function mostrar() As DataTable
+        Try
+            conectado()
+            cmd = New SqlCommand("mostrar_producto")
+            cmd.CommandType = CommandType.StoredProcedure
+
+            cmd.Connection = cnn
+
+            If cmd.ExecuteNonQuery Then
+                Dim dt As New DataTable
+                Dim da As New SqlDataAdapter(cmd)
+                da.Fill(dt)
+                Return dt
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        Finally
+            desconectado()
+        End Try
+    End Function
+
+    Public Function insertar(ByVal objProducto As vproducto) As Boolean
+        Try
+            conectado()
+            cmd = New SqlCommand("insertar_producto")
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Connection = cnn
+
+
+            cmd.Parameters.AddWithValue("@idcategoria", objProducto.gidcategoria)
+            cmd.Parameters.AddWithValue("@nombre", objProducto.gnombre)
+            cmd.Parameters.AddWithValue("@descripcion", objProducto.gdescripcion)
+            cmd.Parameters.AddWithValue("@stock", objProducto.gStock)
+            cmd.Parameters.AddWithValue("@precio_compra", objProducto.gprecio_compra)
+            cmd.Parameters.AddWithValue("@precio_venta", objProducto.gprecio_venta)
+            cmd.Parameters.AddWithValue("@fecha_vencimiento", objProducto.gfecha_vencimiento)
+            cmd.Parameters.AddWithValue("@imagen", objProducto.gimagen)
+
+            If cmd.ExecuteNonQuery Then
+                Return True
+            Else
+                Return False
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+
+        Finally
+            desconectado()
+        End Try
+
+    End Function
+
+    Public Function editar(ByVal objProducto As vproducto) As Boolean
+        Try
+            conectado()
+            cmd = New SqlCommand("editar_producto")
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Connection = cnn
+
+            cmd.Parameters.AddWithValue("@idproducto", objProducto.gidproducto)
+            cmd.Parameters.AddWithValue("@idcategoria", objProducto.gidcategoria)
+            cmd.Parameters.AddWithValue("@nombre", objProducto.gnombre)
+            cmd.Parameters.AddWithValue("@descripcion", objProducto.gdescripcion)
+            cmd.Parameters.AddWithValue("@stock", objProducto.gStock)
+            cmd.Parameters.AddWithValue("@precio_compra", objProducto.gprecio_compra)
+            cmd.Parameters.AddWithValue("@precio_venta", objProducto.gprecio_venta)
+            cmd.Parameters.AddWithValue("@fecha_vencimiento", objProducto.gfecha_vencimiento)
+            cmd.Parameters.AddWithValue("@imagen", objProducto.gimagen)
+
+            If cmd.ExecuteNonQuery Then
+                Return True
+            Else
+                Return False
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+
+        Finally
+            desconectado()
+        End Try
+
+    End Function
+
+    Public Function eliminar(ByVal objProducto As vproducto) As Boolean
+
+        Try
+            conectado()
+            cmd = New SqlCommand("eliminar_producto")
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Connection = cnn
+
+            cmd.Parameters.Add("@idproducto", SqlDbType.NVarChar, 50).Value = objProducto.gidproducto
+
+            If cmd.ExecuteNonQuery Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+
+End Class
---
+++ SistemaVentasVB/sisventas/Presentacion/frmproducto.Designer.vb
@@ -0,0 +1,345 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmproducto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cbo_eliminar = New System.Windows.Forms.CheckBox()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.inexistente = New System.Windows.Forms.Label()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.cboCampo = New System.Windows.Forms.ComboBox()
+        Me.dataListado = New System.Windows.Forms.DataGridView()
+        Me.Eliminar = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDNI = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtApellidos = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNombres = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtIdCliente = New System.Windows.Forms.TextBox()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dataListado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.White
+        Me.GroupBox2.Controls.Add(Me.cbo_eliminar)
+        Me.GroupBox2.Controls.Add(Me.btnEliminar)
+        Me.GroupBox2.Controls.Add(Me.inexistente)
+        Me.GroupBox2.Controls.Add(Me.txtBuscar)
+        Me.GroupBox2.Controls.Add(Me.cboCampo)
+        Me.GroupBox2.Controls.Add(Me.dataListado)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.Color.SteelBlue
+        Me.GroupBox2.Location = New System.Drawing.Point(399, 27)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(377, 395)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Listado de Clientes"
+        '
+        'cbo_eliminar
+        '
+        Me.cbo_eliminar.AutoSize = True
+        Me.cbo_eliminar.Location = New System.Drawing.Point(18, 65)
+        Me.cbo_eliminar.Name = "cbo_eliminar"
+        Me.cbo_eliminar.Size = New System.Drawing.Size(70, 17)
+        Me.cbo_eliminar.TabIndex = 16
+        Me.cbo_eliminar.Text = "Eliminar"
+        Me.cbo_eliminar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.BackColor = System.Drawing.Color.Teal
+        Me.btnEliminar.ForeColor = System.Drawing.Color.White
+        Me.btnEliminar.Location = New System.Drawing.Point(28, 346)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 15
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = False
+        '
+        'inexistente
+        '
+        Me.inexistente.AutoSize = True
+        Me.inexistente.Location = New System.Drawing.Point(131, 210)
+        Me.inexistente.Name = "inexistente"
+        Me.inexistente.Size = New System.Drawing.Size(112, 13)
+        Me.inexistente.TabIndex = 3
+        Me.inexistente.Text = "Datos Inexistentes"
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(169, 28)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(187, 20)
+        Me.txtBuscar.TabIndex = 2
+        '
+        'cboCampo
+        '
+        Me.cboCampo.FormattingEnabled = True
+        Me.cboCampo.Items.AddRange(New Object() {"nombre", "apellidos", "dni", "telefono"})
+        Me.cboCampo.Location = New System.Drawing.Point(18, 28)
+        Me.cboCampo.Name = "cboCampo"
+        Me.cboCampo.Size = New System.Drawing.Size(133, 21)
+        Me.cboCampo.TabIndex = 1
+        Me.cboCampo.Text = "dni"
+        '
+        'dataListado
+        '
+        Me.dataListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataListado.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Eliminar})
+        Me.dataListado.Location = New System.Drawing.Point(18, 88)
+        Me.dataListado.Name = "dataListado"
+        Me.dataListado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dataListado.Size = New System.Drawing.Size(338, 232)
+        Me.dataListado.TabIndex = 0
+        '
+        'Eliminar
+        '
+        Me.Eliminar.HeaderText = "Eliminar"
+        Me.Eliminar.Name = "Eliminar"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnEditar)
+        Me.GroupBox1.Controls.Add(Me.btnCancelar)
+        Me.GroupBox1.Controls.Add(Me.btnGuardar)
+        Me.GroupBox1.Controls.Add(Me.btnNuevo)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtDNI)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtTelefono)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtDireccion)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtApellidos)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtNombres)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtIdCliente)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.SteelBlue
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(351, 396)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Mantenimiento"
+        '
+        'btnEditar
+        '
+        Me.btnEditar.BackColor = System.Drawing.Color.Teal
+        Me.btnEditar.ForeColor = System.Drawing.Color.White
+        Me.btnEditar.Location = New System.Drawing.Point(151, 346)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEditar.TabIndex = 15
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = False
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.Color.Teal
+        Me.btnCancelar.ForeColor = System.Drawing.Color.White
+        Me.btnCancelar.Location = New System.Drawing.Point(249, 346)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 14
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.BackColor = System.Drawing.Color.Teal
+        Me.btnGuardar.ForeColor = System.Drawing.Color.White
+        Me.btnGuardar.Location = New System.Drawing.Point(151, 346)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 13
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = False
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.BackColor = System.Drawing.Color.Teal
+        Me.btnNuevo.ForeColor = System.Drawing.Color.White
+        Me.btnNuevo.Location = New System.Drawing.Point(41, 346)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 12
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(38, 288)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "DNI"
+        '
+        'txtDNI
+        '
+        Me.txtDNI.Location = New System.Drawing.Point(172, 281)
+        Me.txtDNI.Name = "txtDNI"
+        Me.txtDNI.Size = New System.Drawing.Size(152, 20)
+        Me.txtDNI.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(38, 239)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Telefono"
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(172, 232)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(152, 20)
+        Me.txtTelefono.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 190)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Direccion"
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(172, 183)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(152, 20)
+        Me.txtDireccion.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Apellidos"
+        '
+        'txtApellidos
+        '
+        Me.txtApellidos.Location = New System.Drawing.Point(172, 134)
+        Me.txtApellidos.Name = "txtApellidos"
+        Me.txtApellidos.Size = New System.Drawing.Size(152, 20)
+        Me.txtApellidos.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nombres"
+        '
+        'txtNombres
+        '
+        Me.txtNombres.Location = New System.Drawing.Point(172, 88)
+        Me.txtNombres.Name = "txtNombres"
+        Me.txtNombres.Size = New System.Drawing.Size(152, 20)
+        Me.txtNombres.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "IdCliente"
+        '
+        'txtIdCliente
+        '
+        Me.txtIdCliente.Location = New System.Drawing.Point(172, 43)
+        Me.txtIdCliente.Name = "txtIdCliente"
+        Me.txtIdCliente.Size = New System.Drawing.Size(152, 20)
+        Me.txtIdCliente.TabIndex = 0
+        '
+        'frmproducto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmproducto"
+        Me.Text = "frmproducto"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.dataListado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox2 As Windows.Forms.GroupBox
+    Friend WithEvents cbo_eliminar As Windows.Forms.CheckBox
+    Friend WithEvents btnEliminar As Windows.Forms.Button
+    Friend WithEvents inexistente As Windows.Forms.Label
+    Friend WithEvents txtBuscar As Windows.Forms.TextBox
+    Friend WithEvents cboCampo As Windows.Forms.ComboBox
+    Friend WithEvents dataListado As Windows.Forms.DataGridView
+    Friend WithEvents Eliminar As Windows.Forms.DataGridViewCheckBoxColumn
+    Friend WithEvents GroupBox1 As Windows.Forms.GroupBox
+    Friend WithEvents btnEditar As Windows.Forms.Button
+    Friend WithEvents btnCancelar As Windows.Forms.Button
+    Friend WithEvents btnGuardar As Windows.Forms.Button
+    Friend WithEvents btnNuevo As Windows.Forms.Button
+    Friend WithEvents Label6 As Windows.Forms.Label
+    Friend WithEvents txtDNI As Windows.Forms.TextBox
+    Friend WithEvents Label5 As Windows.Forms.Label
+    Friend WithEvents txtTelefono As Windows.Forms.TextBox
+    Friend WithEvents Label4 As Windows.Forms.Label
+    Friend WithEvents txtDireccion As Windows.Forms.TextBox
+    Friend WithEvents Label3 As Windows.Forms.Label
+    Friend WithEvents txtApellidos As Windows.Forms.TextBox
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents txtNombres As Windows.Forms.TextBox
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents txtIdCliente As Windows.Forms.TextBox
+End Class
