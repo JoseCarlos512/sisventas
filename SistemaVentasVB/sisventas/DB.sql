6bf9d464e51943cde0a6adf9cd40515fedd1c725
ljose <ljose@DESKTOP-7NFI325>
2023-04-30 22:03:28 -0500

    add procedures by ventas y detalle_venta

--- SistemaVentasVB/sisventas/DB.sql
+++ SistemaVentasVB/sisventas/DB.sql
@@ -223,3 +223,117 @@ create proc eliminar_producto
 as
 delete from producto where idproducto=@idproducto
 go
+
+-- VENTA
+select * from ventas
+go
+
+create proc insertar_venta
+@idcliente as integer,
+@fecha_venta as date,
+@tipo_documento as varchar(50),
+@num_documento as varchar(50)
+as
+insert into ventas (idcliente, fecha_venta, tipo_documento, num_documento)
+values(@idcliente, @fecha_venta, @tipo_documento, @num_documento)
+go
+
+create proc editar_venta
+@idventa as integer,
+@idcliente as integer,
+@fecha_venta as date,
+@tipo_documento as varchar(50),
+@num_documento as varchar(50)
+as
+update ventas 
+set idcliente=@idcliente,fecha_venta=@fecha_venta,tipo_documento=@tipo_documento,
+	num_documento=@num_documento 
+where idventa = @idventa
+go
+
+create proc eliminar_venta
+@idventa as integer 
+as
+delete from ventas where idventa=@idventa
+go
+
+
+create proc mostrar_venta
+as
+select 
+ventas.idventa, ventas.idcliente, cliente.apellidos, cliente.dni, ventas.fecha_venta, 
+ventas.tipo_documento, ventas.num_documento
+from ventas
+inner join cliente
+on ventas.idcliente = cliente.idcliente
+go
+
+-- DETALLE VENTA
+select * from detalle_venta
+go
+
+create proc insertar_detalle_venta
+@idventa as integer,
+@idproducto as integer,
+@cantidad as decimal(18,2),
+@precio_unitario as decimal(18,2)
+as
+insert into detalle_venta(idventa,idproducto,cantidad,precio_unitario)
+values(@idventa,@idproducto,@cantidad,@precio_unitario)
+go
+
+create proc editar_detalle_venta
+@oddetalle_venta as integer,
+@idventa as integer,
+@idproducto as integer,
+@cantidad as decimal(18,2),
+@precio_unitario as decimal(18,2)
+as
+update detalle_venta 
+set idventa=@idventa,idproducto=@idproducto,cantidad=@cantidad,precio_unitario=@precio_unitario
+where iddetalle_venta=iddetalle_venta
+go
+
+create proc eliminar_detalle_venta
+@iddetalle_venta as integer
+as
+delete from detalle_venta 
+where iddetalle_venta=@iddetalle_venta
+go
+
+create proc mostrar_detalle_venta
+as
+select * from detalle_venta order by iddetalle_venta desc
+go
+
+create proc aumentar_stock
+@idproducto as integer,
+@cantidad as decimal(18,2)
+as
+update producto set stock=stock+@cantidad
+where idproducto=@idproducto
+go
+
+create proc disminuir_stock
+@idproducto as integer,
+@cantidad as decimal(18,2)
+as
+update producto set stock=stock-@cantidad
+where idproducto=@idproducto
+go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
